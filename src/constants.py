bd6eb2ca9ffa04ee4b57e8d8aa88cbe90d50235e
Charl_10 <134136374+charlos1002@users.noreply.github.com>
2025-05-18 23:40:19 +0700

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -7,8 +7,10 @@ SCREEN_HEIGHT = 600
 
 # Physics
 GRAVITY           = 0.5
+GRAVITY_BOOST     = 1.5
 BOB_SPEED         = 3.5
 MOVABLE_WALL_SPEED = 3.5
+JUMP_STRENGTH     = -12
 
 # Colors (R, G, B)
 BLACK    = (0,   0,   0)
