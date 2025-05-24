aed14feb5a53d9860f328cfaac523e85cb1976fa
Daaccd <brahmantio05@gmail.com>
2025-05-24 12:27:44 +0700

    Add game logo

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import pygame
-from src.constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import pygame, os
+from src.constants import SCREEN_WIDTH, SCREEN_HEIGHT, IMAGES_DIR
 from src.game import Game
 
 def main():
     pygame.init()
+    try:
+        icon_path = os.path.join(IMAGES_DIR, "kurakura.png")
+        game_icon = pygame.image.load(icon_path)
+        pygame.display.set_icon(game_icon)
+    except pygame.error as e:
+        print(f"Tidak dapat memuat ikon: {icon_path} - {e}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Agus Turtle")
     game = Game(screen)
